54e41730416823bc0fd39dec974c0ee55d1e5343
Andrea Barghigiani <andrea@codeat.it>
2017-03-13 10:49:58 +0100

    Lezione 5 - Aree footer in base al valore nel Customizer

--- functions.php
+++ functions.php
@@ -83,3 +83,31 @@ function skam_scripts() {
     wp_enqueue_style( 'skam-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'skam_scripts' );
+
+//* Aggiungo le impostazioni del Customizer per le widget area del footer
+function skam_impostazioni( $wp_customize ) {
+
+    $wp_customize->add_section( 'theme_tools', array(
+        'title' => __( 'Personalizza il tema', 'skam' ),
+        'description' => __( 'Utilizza le opzioni incluse per personalizzare il tema.' ),
+        'priority' => 160,
+    ));
+
+    $wp_customize->add_setting( 'col_footer', array(
+        'default' => 2,
+    ));
+
+    $wp_customize->add_control( 'col_footer', array(
+        'label' => __( 'Colonne Footer', 'skam' ),
+        'description' => __( 'Seleziona dal menu a tendina il numero di colonne da usare.', 'skam' ),
+        'section' => 'theme_tools',
+        'type' => 'select',
+        'choices' => array( //Utile per impostare dei limiti
+            '1' => 1,
+            '2' => 2,
+            '3' => 3,
+            '4' => 4
+        ),
+     ));
+}
+add_action( 'customize_register', 'skam_impostazioni' );
